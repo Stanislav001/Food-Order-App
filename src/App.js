9014e35c0231e4341ce2ffcdd54b5a8fddf518da
S.Stanchev <stanchev1002@gmail.com>
2022-06-05 14:52:29 +0300

    Update app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useState, useContext } from "react";
+
 import Header from "./components/Header/Header";
 import Meals from "./components/Meals/Meals";
-
+import MyCart from "./UI/MyCart/MyCart";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
+  const [myCartIsShow, setMyCartIsShow] = useState(false);
+
+  const showMyCartHandler = () => {
+    setMyCartIsShow(true);
+  }
+
+  const hideMyCartHandler = () => {
+    setMyCartIsShow(false);
+  }
+
   return (
-    <div className="App">
-      <Header />
-      <Meals />
-    </div>
+    <CartProvider>
+      {myCartIsShow && <MyCart onClose={hideMyCartHandler} />} // if true this is rendering
+      <Header onClickMyCart={showMyCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
