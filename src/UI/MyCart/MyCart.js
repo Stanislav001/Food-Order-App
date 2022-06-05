657371e2c4dfc917299d1f416826beb2940e7f4f
S.Stanchev <stanchev1002@gmail.com>
2022-06-05 14:36:24 +0300

    Update MyCart.js

--- src/UI/MyCart/MyCart.js
+++ src/UI/MyCart/MyCart.js
@@ -1,27 +1,25 @@
+import Modal from '../Modal/Modal';
 import './MyCart.css';
 
 const MyCart = (props) => {
-    const cartItems = <ul>{[{ id: 'c1', name: 'das', amount: 2, price: 2312 }].map(item => {
-        <li>{item.name}</li>)
-    }</ul>
-};
+    const cartItems = <ul>{[{ id: 'pesho', name: 'dasdsa' }].map(item => {
+        <li>{item.name}</li>
+    })}</ul>
 
-return (
-    <div className='total'>
-        {cartItems}
-        <div>
-            <span>Total Amount: </span>
-        </div>
-        <div>
-            <span>40.50</span>
-        </div>
-
-        <div className='actions'>
-            <button className='button--alt'>Close</button>
-            <button className='button'>Order</button>
-        </div>
-    </div>
-);
+    return (
+        <Modal onClose={props.onClose}>
+            {cartItems}
+            <div className='total'>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className='actions'>
+                <button className='button--alt' onClick={props.onClose}>
+                    Close
+                </button>
+                <button className='button'>Order</button>
+            </div>
+        </Modal>
+    );
 }
-
 export default MyCart;
