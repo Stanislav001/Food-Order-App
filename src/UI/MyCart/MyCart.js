8cbdfaafcf08480b8cdb08873c677d9596bec0be
S.Stanchev <stanchev1002@gmail.com>
2022-06-06 13:09:50 +0300

    Update

--- src/UI/MyCart/MyCart.js
+++ src/UI/MyCart/MyCart.js
@@ -7,30 +7,29 @@ import Modal from '../Modal/Modal';
 import './MyCart.css';
 
 const MyCart = (props) => {
-    const cartContext = useContext(CartContext);
+    const cartCtx = useContext(CartContext);
 
-    const totalAmount = `${cartContext.totalAmount.toFixed(2)}$`;
-    const hasItems = cartContext.items.length > 0;
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
 
     const cartItemRemoveHandler = (id) => {
-
+        cartCtx.removeItem(id);
     };
 
     const cartItemAddHandler = (item) => {
-
+        cartCtx.addItem({ ...item, amount: 1 });
     };
-
 
     const cartItems = (
         <ul className='cart-items'>
-            {cartContext.items.map((item) => (
+            {cartCtx.items.map((item) => (
                 <CartItem
-                    key={item.key}
+                    key={item.id}
                     name={item.name}
                     amount={item.amount}
                     price={item.price}
-                    onRemove={cartItemRemoveHandler}
-                    onAdd={cartItemAddHandler}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
                 />
             ))}
         </ul>
@@ -51,5 +50,6 @@ const MyCart = (props) => {
             </div>
         </Modal>
     );
-}
+};
+
 export default MyCart;
