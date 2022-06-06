80f01760c0987ff9b6c77f8002c13b5f88704451
S.Stanchev <stanchev1002@gmail.com>
2022-06-06 11:14:44 +0300

    Update MyCart Component
    Use useContext hook

--- src/UI/MyCart/MyCart.js
+++ src/UI/MyCart/MyCart.js
@@ -1,23 +1,53 @@
+import { useContext } from 'react';
+
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem/CartItem';
+
 import Modal from '../Modal/Modal';
 import './MyCart.css';
 
 const MyCart = (props) => {
-    const cartItems = <ul>{[{ id: 'pesho', name: 'dasdsa' }].map(item => {
-        <li>{item.name}</li>
-    })}</ul>
+    const cartContext = useContext(CartContext);
+
+    const totalAmount = `${cartContext.totalAmount.toFixed(2)}$`;
+    const hasItems = cartContext.items.length > 0;
+
+    const cartItemRemoveHandler = (id) => {
+
+    };
+
+    const cartItemAddHandler = (item) => {
+
+    };
+
+
+    const cartItems = (
+        <ul className='cart-items'>
+            {cartContext.items.map((item) => (
+                <CartItem
+                    key={item.key}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler}
+                    onAdd={cartItemAddHandler}
+                />
+            ))}
+        </ul>
+    );
 
     return (
         <Modal onClose={props.onClose}>
             {cartItems}
             <div className='total'>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className='actions'>
                 <button className='button--alt' onClick={props.onClose}>
                     Close
                 </button>
-                <button className='button'>Order</button>
+                {hasItems && <button className='button'>Order</button>}
             </div>
         </Modal>
     );
