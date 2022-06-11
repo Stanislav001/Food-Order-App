c68e5adca1e20b0f70dbd6df0723daa4390304bc
S.Stanchev <stanchev1002@gmail.com>
2022-06-11 09:45:47 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import About from './pages/About';
+import NotFound from './pages/NotFound';
 import ContactUs from './pages/ContactUs';
+import Footer from './pages/Footer/Footer';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 import './index.css';
 import MainNavigation from './components/Header/MainNavigation/MainNavigation';
@@ -20,6 +24,11 @@ root.render(
       <Route path="/" element={<App />} />
       <Route path="/about" element={<About />} />
       <Route path="/contactUs" element={<ContactUs />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
+
+    <Footer />
   </BrowserRouter>
 );
