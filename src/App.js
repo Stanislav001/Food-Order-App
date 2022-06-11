27027d8082674dfaafa2b82d50f70e414105b65e
S.Stanchev <stanchev1002@gmail.com>
2022-06-11 11:52:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MyCart from "./UI/MyCart/MyCart";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Header/Header";
 import CartProvider from "./store/CartProvider";
+import MainNavigation from "./components/Header/MainNavigation/MainNavigation";
 
 export default function App() {
   const [myCartIsShow, setMyCartIsShow] = useState(false);
@@ -18,6 +19,8 @@ export default function App() {
 
   return (
     <CartProvider>
+      <MainNavigation />
+
       {myCartIsShow && <MyCart onClose={hideMyCartHandler} />}
       <Header onClickMyCart={showMyCartHandler} />
 
