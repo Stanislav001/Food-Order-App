903a3f3087887b623e258ea793104b4ca358534d
S.Stanchev <stanchev1002@gmail.com>
2022-06-12 12:03:19 +0300

    Add my providers in index.js

--- src/index.js
+++ src/index.js
@@ -6,29 +6,35 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import About from './pages/About';
-import Login from './pages/Login/Login';
+import AuthPage from './pages/AuthPage/AuthPage';
 import NotFound from './pages/NotFound';
 import ContactUs from './pages/ContactUs';
 import Footer from './pages/Footer/Footer';
-import Register from './pages/Register/Register';
 import MealDetails from './pages/MealDetail/MealDetail';
+
+import { AuthContextProvider } from './store/auth-context';
+import CartProvider from './store/CartProvider';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <AuthContextProvider>
+    <CartProvider>
+      <BrowserRouter>
 
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
-      <Route path='/meals/:id' element={<MealDetails />} />
-      <Route path="/contactUs" element={<ContactUs />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path='/meals/:id' element={<MealDetails />} />
+          <Route path="/contactUs" element={<ContactUs />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-    <Footer />
-  </BrowserRouter>
+        <Footer />
+      </BrowserRouter>
+    </CartProvider>
+  </AuthContextProvider>
+
 );
