31c2f231fc7afa17a1b27e50c209eafb6eddf244
S.Stanchev <stanchev1002@gmail.com>
2022-06-12 10:00:34 +0300

    Add auth context
    I will use this auth context with use useContext() hook

---
+++ src/store/auth-context.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const AuthContext = React.createContext({
+    toke: '',
+    isLoggedIn: false,
+    login: (token) => { },
+    logout: () => { }
+});
+
+export default AuthContext;
+
+export const AuthContextProvider = (props) => {
+    const [token, setToken] = useState(null);
+
+    const userIsLoggedIn = !!token;
+
+    function loginHandler(token) {
+        setToken(token);
+    };
+
+    function logoutHandler() {
+        setToken(null);
+    }
+
+    const contextValue = {
+        token: token,
+        isLoggedIn: userIsLoggedIn,
+        login: loginHandler,
+        logout: logoutHandler,
+    };
+
+    return (
+        <AuthContextProvider value={contextValue}>
+            {props.children}
+        </AuthContextProvider>
+    )
+}
