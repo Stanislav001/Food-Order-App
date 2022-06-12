99cd4e520062ddbdec52c98784aa94c29be1bcf0
S.Stanchev <stanchev1002@gmail.com>
2022-06-12 12:04:25 +0300

    Update my Header Component
    Hide some links from unlogged in users

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,25 @@
+import { useContext } from 'react';
+
 import HeaderCartButton from './HeaderCartButton/HeaderCartButton';
 import MainNavigation from "./MainNavigation/MainNavigation";
+
+import AuthContext from '../../store/auth-context';
 
 import './Header.css';
 
 import mealsImage from '../../assets/meals.jpg';
 
 export default function Header({ onClickMyCart }) {
+    const authContext = useContext(AuthContext);
+
+    const userIsLoggedIn = authContext.isLoggedIn;
+
     return (
         <>
             <header className='header'>
                 <h1>Stas_Panda</h1>
                 <MainNavigation />
-                <HeaderCartButton onClick={onClickMyCart} />
+                {userIsLoggedIn && (<HeaderCartButton onClick={onClickMyCart} />)}
             </header>
 
             <div className='main-image'>
