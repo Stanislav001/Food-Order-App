a5bbabc579397250d5be25b0cf12fa8066e0b769
S.Stanchev <stanchev1002@gmail.com>
2022-06-12 11:03:24 +0300

    Update Navigation Component

--- src/components/Header/MainNavigation/MainNavigation.js
+++ src/components/Header/MainNavigation/MainNavigation.js
@@ -1,22 +1,37 @@
-import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+import AuthContext from '../../../store/auth-context';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+    const navigate = useNavigate();
+    const authContext = useContext(AuthContext);
+    const userIsLoggedIn = authContext.isLoggedIn;
+
+    function logoutHandler() {
+        authContext.logout();
+        navigate('/');
+    }
+
     return (
         <header className={classes.header}>
             <nav className={classes.nav}>
                 <ul>
-                    <li>
-                        <NavLink to='/register' className={navData => navData.isActive ? classes.active : ''}>
-                            Register
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/login' className={navData => navData.isActive ? classes.active : ''}>
-                            Login
-                        </NavLink>
-                    </li>
+                    {!userIsLoggedIn && (
+                        <li>
+                            <NavLink to='/auth' className={navData => navData.isActive ? classes.active : ''}>
+                                Login
+                            </NavLink>
+                        </li>
+                    )}
+
+                    {userIsLoggedIn && (
+                        <li>
+                            <button onClick={logoutHandler}>Logout</button>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
